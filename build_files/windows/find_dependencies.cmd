585832f8c2c2147e774f80f34ff65b4ec21aed96
filispeen <72784386+filispeen@users.noreply.github.com>
2024-06-30 00:22:50 +0300

    Update find_dependencies.cmd

--- build_files/windows/find_dependencies.cmd
+++ build_files/windows/find_dependencies.cmd
@@ -7,11 +7,6 @@ REM For python, default on 310 but if that does not exist also check
 REM the 311, 312 and finally 39 folders to see if those are there, it checks
 REM this far ahead to ensure good lib folder compatibility in the future
 REM it falls back to 3.9 just incase it is a very old lib folder.
-echo CHECK_FIL=%BLENDER_DIR%
-dir %BLENDER_DIR%
-dir %BLENDER_DIR%../
-dir %BLENDER_DIR%../../
-dir %BLENDER_DIR%../../../
 echo %BLENDER_DIR%
 set PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\310\bin\python.exe
 dir %PYTHON%
